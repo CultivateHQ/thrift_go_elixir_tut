c111e90661e383fd69751505a313b46deb14cb91
Dan Munckton <dan@cultivatehq.com>
2018-04-03 15:55:24 +0100

    Refactor tests

--- ex-client/test/db_test.exs
+++ ex-client/test/db_test.exs
@@ -7,25 +7,35 @@ defmodule DbTest do
     %{}
   end
 
-  test "records can be added, retrieved and removed" do
-    entry_a = %{"one" => :two, 3 => "four"}
-    entry_b = %{"x" => :y, 1 => "two"}
+  @entry_a %{"one" => :two, 3 => "four"}
+  @entry_b %{"x" => :y, 1 => "two"}
 
-    :ok = Db.create(entry_a)
-    :ok = Db.create(entry_b)
+  test "records can be added" do
+    :ok = Db.create(@entry_a)
+    :ok = Db.create(@entry_b)
 
-    assert(Db.all() == [entry_a, entry_b])
+    assert(Db.all() == [@entry_a, @entry_b])
+  end
 
-    assert(Db.find(0) == entry_a)
-    assert(Db.find(1) == entry_b)
+  test "records can be retrieved" do
+    :ok = Db.create(@entry_a)
+    :ok = Db.create(@entry_b)
 
-    assert(Db.remove(1) == entry_b)
+    assert(Db.find(0) == @entry_a)
+    assert(Db.find(1) == @entry_b)
+  end
+
+  test "records can be removed" do
+    :ok = Db.create(@entry_a)
+    :ok = Db.create(@entry_b)
+
+    assert(Db.remove(1) == @entry_b)
     assert(Db.remove(1) == :not_found)
 
-    assert(Db.find(0) == entry_a)
+    assert(Db.find(0) == @entry_a)
     assert(Db.find(1) == :not_found)
 
-    :ok = Db.create(entry_b)
-    assert(Db.find(2) == entry_b)
+    :ok = Db.create(@entry_b)
+    assert(Db.find(2) == @entry_b)
   end
 end
